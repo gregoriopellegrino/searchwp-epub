1fea5610a2892cb82b18610599c7c84e573006fc
root <root@vps798322.ovh.net>
2020-05-09 21:53:50 +0200

    Fixed in live enviroment

--- search-wp.php
+++ search-wp.php
@@ -8,7 +8,7 @@
 	Plugin Name: SearchWP EPUB
 	Plugin URI:  https://github.com/gregoriopellegrino/searchwp-epub
 	Description: WordPress plugin that enables indexing of EPUBs in SearchWP
-	Version:     0.1
+	Version:     0.2
 	Author:      Gregorio Pellegrino
 	Author URI:  https://effata.it
 	Text Domain: search-wp
@@ -22,19 +22,31 @@
 
 	add_action('searchwp_index_post', function($post) {
 		if ( 'application/epub+zip' === $post->post_mime_type ) {
-			$document_content = get_post_meta( $post->ID, SEARCHWP_PREFIX . 'content', true );
-			
-			if ( empty( $document_content ) ) {
+			if ( !metadata_exists( 'post', $post->ID, SEARCHWP_PREFIX . 'content' ) ) {
 				require __DIR__ . '/vendor/autoload.php';
-				$client = \Vaites\ApacheTika\Client::make('tika-app-1.24.1.jar');
+				$client = \Vaites\ApacheTika\Client::make(__DIR__ .'/tika-app-1.24.1.jar');
 			
 				$filename = get_attached_file( absint( $post->ID ) );
 			
 				$document_content = $client->getText($filename);
-			
+        
 				$document_content = sanitize_text_field( $document_content );
-				delete_post_meta( $post->ID, SEARCHWP_PREFIX . 'content' );
 				update_post_meta( $post->ID, SEARCHWP_PREFIX . 'content', $document_content );
 			}
 		}
 	});
+
+add_filter('searchwp_get_custom_fields', function($custom, $post_id) {
+  if(get_post_mime_type($post_id) === 'application/epub+zip') {
+    require __DIR__ . '/vendor/autoload.php';
+		$client = \Vaites\ApacheTika\Client::make(__DIR__ .'/tika-app-1.24.1.jar');
+    
+    $filename = get_attached_file( absint( $post->ID ) );
+    
+    $metadata = (array) $client->getMetadata($filename)->meta;
+	  unset($metadata["X-Parsed-By"]);
+    
+    $custom["searchwp_epub_metadata"] = $metadata;
+  }
+  return $custom;
+}, 10, 2);
